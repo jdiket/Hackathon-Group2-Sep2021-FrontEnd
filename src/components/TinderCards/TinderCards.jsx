d8d4da6aebe2818b8686652c9dee2b41f9671414
alfranc <alexfranco12@gmail.com>
2021-09-22 13:58:53 -0500

    rebase changes

---
+++ src/components/TinderCards/TinderCards.jsx
@@ -0,0 +1,62 @@
+import './tindercards.css'
+import TinderCard from 'react-tinder-card'
+import dog from '../../images/example.jpeg'
+
+// Material UI Imports
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+const onSwipe = (direction) => {
+  console.log('You swiped: ' + direction)
+}
+
+const onCardLeftScreen = (myIdentifier) => {
+  console.log(myIdentifier + ' left the screen')
+}
+
+const TinderCards = () => {
+  return ( 
+    <div className="TinderCard">
+      <TinderCard
+        onSwipe={onSwipe}
+        onCardLeftScreen={() => onCardLeftScreen('fooBar')}
+        preventSwipe={['right', 'left']}
+        > 
+         <Card className="card" sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="240"
+              image={dog}
+              alt="dog"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lorem ipsum dolor
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quam velit dolorem laboriosam magni aspernatur corporis suscipit, enim nam, temporibus explicabo id sit neque necessitatibus iure facilis? Quia, molestiae sunt!
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </TinderCard>
+
+ 
+      <Stack className="card__buttons" spacing={4} direction="row">
+        <Button variant="outlined">yes</Button>
+        <Button variant="outlined">more info</Button>
+        <Button variant="outlined">no</Button>
+      </Stack>
+    
+    </div>
+   );
+}
+ 
+export default TinderCards;
---
+++ src/pages/BookmarkPage/BookmarkPage.jsx
@@ -0,0 +1,15 @@
+const BookmarkPage = () => {
+  return ( 
+    <div className="BookmarkPage">
+      <h1 className="header">Bookmark Page</h1>
+      <ul className="list">
+        <li>dog 1</li>
+        <li>dog 2</li>
+        <li>dog 3</li>
+        <li>dog 4</li>
+      </ul>
+    </div>
+   );
+}
+ 
+export default BookmarkPage;

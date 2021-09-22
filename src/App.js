14f46a45793c9db91d870350f019218a4337a998
alfranc <alexfranco12@gmail.com>
2021-09-22 08:32:42 -0500

    added in css and deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
-import { Route, Switch, Redirect, Link } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar'
 import AboutPage from './pages/AboutPage/AboutPage'
 
-function App() {
+const App = () => {
   return (
-    <div>
+    <div className="App">
       <NavBar />
+
       <Switch>
+        <Route exact path="/">
+          <h1>Homepage</h1>
+        </Route>
         <Route path="/about">
           <AboutPage />
         </Route>
         <Route path="/find">
           <h1>Find Page</h1>
         </Route>
-        <Route path="/">
-          <h1>homepage</h1>
-        </Route>
+
         <Redirect to="/" />
       </Switch>
     </div>
